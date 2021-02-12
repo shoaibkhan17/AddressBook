420289670fef6d388ccb96740676593926cb6213
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-02-11 21:18:30 -0500

    Step 1 testing

---
+++ src/test/java/App/WebTest.java
@@ -0,0 +1,47 @@
+package App;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+
+@SpringBootTest
+public class WebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void buddy1NameTest() throws Exception {
+        this.mockMvc.perform(get("/AddressBook")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Shoaib")));
+    }
+
+    @Test
+    public void buddy2NameTest() throws Exception {
+        this.mockMvc.perform(get("/AddressBook")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Omar")));
+    }
+
+    @Test
+    public void buddy1AddressTest() throws Exception {
+        this.mockMvc.perform(get("/AddressBook")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("123 water road")));
+    }
+
+    @Test
+    public void buddy2AddressTest() throws Exception {
+        this.mockMvc.perform(get("/AddressBook")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("153 water road")));
+    }
+}
